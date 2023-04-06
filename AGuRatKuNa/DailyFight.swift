ed0b93fdd23accdf7da4dfa18e0feb1f094ef73b
GaOn <xnoag@icloud.com>
2023-04-06 18:05:27 +0900

    DailyFight_Create DailyFight Struct

---
+++ AGuRatKuNa/DailyFight.swift
@@ -0,0 +1,14 @@
+//
+//  DailyFight.swift
+//  AGuRatKuNa
+//
+//  Created by xnoag on 2023/04/06.
+//
+
+import Foundation
+
+struct DailyFight {
+    var title: String
+    var fightTimes: Int
+    var fighters: [String]
+}
