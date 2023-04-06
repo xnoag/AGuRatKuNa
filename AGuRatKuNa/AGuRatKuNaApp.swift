e580fb433f9afac01d7f7d82344cfadf5173714c
GaOn <xnoag@icloud.com>
2023-04-06 17:36:28 +0900

    Initial Commit

---
+++ AGuRatKuNa/AGuRatKuNaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGuRatKuNaApp.swift
+//  AGuRatKuNa
+//
+//  Created by xnoag on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct AGuRatKuNaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
