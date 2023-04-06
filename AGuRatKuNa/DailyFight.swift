b8a06e8d69c69dff54813aa9613ed5f52c1cb061
GaOn <xnoag@icloud.com>
2023-04-06 18:27:10 +0900

    Theme_Create Theme Struct

--- AGuRatKuNa/DailyFight.swift
+++ AGuRatKuNa/DailyFight.swift
@@ -3,12 +3,14 @@
 //  AGuRatKuNa
 //
 //  Created by xnoag on 2023/04/06.
-//
+//  View들 사이에 보여줄 정보에 대한 구조체를 새로이 정리한다.
 
 import Foundation
 
 struct DailyFight {
+    // 이 데이터들을 가지고 정보를 뷰마다 주고받고 할 거임.
     var title: String
     var fightTimes: Int
     var fighters: [String]
+    // Theme 프로퍼티도 저장을 할 건데, Theme 타입이 정의가 안 되어 있기 때문에 정의해준다.
 }
---
+++ AGuRatKuNa/Theme.swift
@@ -0,0 +1,35 @@
+//
+//  Theme.swift
+//  AGuRatKuNa
+//
+//  Created by xnoag on 2023/04/06.
+//  DailyFight 구조체에 들어갈 Theme을 정의해준다.
+
+import Foundation
+// Color는 SwiftUI Framework에서 제공하는 기능이므로 import 해준다.
+import SwiftUI
+
+enum Theme: String {
+    // 이런 색상들을 쓸거다.
+    case gaonRed
+    case gaonYellow
+    case gaonGreen
+    case gaonBlue
+    
+    // 화면 배경에 띌 메인 색상을 정의한다.
+    var mainColor: Color {
+        // Color는 rawValue로 접근할 수 있다.
+        Color(rawValue)
+    }
+    
+    // 화면 배경색에 맞는 accentColor 정의
+    var accentColor: Color {
+        // Theme이 이거일 땐 .white, 저거일 땐 .black을 Return 해주게 한다.
+        switch self {
+        case .gaonRed, .gaonGreen, .gaonBlue:
+            return .white
+        case .gaonYellow:
+            return .black
+        }
+    }
+}
