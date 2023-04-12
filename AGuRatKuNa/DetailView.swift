b1e3264284b0370e5d6642fc9002167724c61692
GaOn <xnoag@icloud.com>
2023-04-12 17:09:11 +0900

    DetailView_HISTORY Created

--- AGuRatKuNa/DetailView.swift
+++ AGuRatKuNa/DetailView.swift
@@ -44,7 +44,7 @@ struct DetailView: View {
             
             // History Section
             Section(header: Text("History")) {
-                
+                Label("No Fighters Yet", systemImage: "calendar.badge.exclamationmark")
             }
             
         }
