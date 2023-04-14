3ec9dbb4e6e6ea5a4774267e8581c6d4ba7659f5
GaOn <xnoag@icloud.com>
2023-04-14 17:22:19 +0900

    DetailEditView Created

---
+++ AGuRatKuNa/DetailEditView.swift
@@ -0,0 +1,29 @@
+//
+//  DetailEditView.swift
+//  AGuRatKuNa
+//
+//  Created by xnoag on 2023/04/14.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        List {
+            // FIGHTING INFO Section
+            Section(header: Text("FIGHTING INFO")) {
+                
+            }
+            // FIGHTERS Section
+            Section {
+                
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
