f76373bba25b1e725c024cd6aba9d6fbf58cc8ba
GaOn <xnoag@icloud.com>
2023-04-24 23:02:45 +0900

    FightingHeaderView_BasicBuilding

--- AGuRatKuNa/FightingView/FightingHeaderView.swift
+++ AGuRatKuNa/FightingView/FightingHeaderView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct FightingHeaderView: View {
+    
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(secondsRemaining)
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressView(value: progress)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                        .labelStyle(.trailingIcon)
+                }
+            }
+        }
+        .padding()
     }
 }
 
 struct FightingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        FightingHeaderView()
+        FightingHeaderView(secondsElapsed: 400, secondsRemaining: 600)
     }
 }
